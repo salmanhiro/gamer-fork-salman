b1a91533486949281ee21297ade4530ff6263291
Salman Al Farisi (salman@gapp.nthu.edu.tw) <salman@eureka00.gpucluster.calab>
2025-07-28 14:25:01 +0800

    Fix: Remove tracers

--- src/TestProblem/Hydro/ParticleStellarStream/Par_Init_ByFunction_ParticleStellarStream.cpp
+++ src/TestProblem/Hydro/ParticleStellarStream/Par_Init_ByFunction_ParticleStellarStream.cpp
@@ -9,7 +9,6 @@
 extern int    ParStream_NPar[3];
 extern double ParStream_Point_Mass;
 extern double ParStream_Par_Sep;
-extern bool   ParStream_Use_Tracers;
 extern bool   ParStream_Use_Massive;
 
 
@@ -66,10 +65,14 @@ void Par_Init_ByFunction_ParticleStellarStream( const long NPar_ThisRank, const 
 
    if ( MPI_Rank == 0 )    Aux_Message( stdout, "%s ...\n", __FUNCTION__ );
 
+   const int Stream_Nx = 100;
+   const int Stream_Nz = 10;
+   const int NStream   = Stream_Nx * Stream_Nz;
+   long NPar_All       = NStream;
 
-   long NPar_All = 0;
-   if ( ParStream_Use_Massive ) NPar_All += 2;
-   if ( ParStream_Use_Tracers ) NPar_All += ParStream_NPar[0]*ParStream_NPar[1]*ParStream_NPar[2];
+if ( NPar_All != NPar_AllRank )
+    Aux_Error( ERROR_INFO, "total number of particles found [%ld] != expect [%ld] !!\n",
+               NPar_All, NPar_AllRank );
 
    if ( NPar_All != NPar_AllRank )
       Aux_Error( ERROR_INFO, "total number of particles found [%ld] != expect [%ld] !!\n",
@@ -98,10 +101,6 @@ void Par_Init_ByFunction_ParticleStellarStream( const long NPar_ThisRank, const 
       long p = 0;
 
       if ( ParStream_Use_Massive ) {
-
-         const int    Stream_Nx        = ParStream_NPar[0];
-         const int    Stream_Nz        = ParStream_NPar[2];
-         const int    NStream          = Stream_Nx * Stream_Nz;
 
          const double Stream_Length    = 100.0;  // kpc along X
          const double Stream_Thickness = 20.0;   // kpc in Z
@@ -162,75 +161,10 @@ void Par_Init_ByFunction_ParticleStellarStream( const long NPar_ThisRank, const 
          #endif
       }
  // if ( ParStream_Use_Massive )
-
-      if ( ParStream_Use_Tracers ) {
-
-   const int    Stream_Nx        = ParStream_NPar[0];
-   const int    Stream_Nz        = ParStream_NPar[2];
-   const int    NStream          = Stream_Nx * Stream_Nz;
-
-   const double Stream_Length    = 100.0;  // kpc along X
-   const double Stream_Thickness = 20.0;    // kpc in Z
-   const double Stream_Height    = 20.0;    // kpc in Y
-
-   const double dx = Stream_Length / Stream_Nx;
-
-   const double x0 = 0.5 * amr->BoxSize[0] - 0.5 * Stream_Length;
-   const double y0 = 0.5 * amr->BoxSize[1];  // fixed central Y
-   const double z0 = 0.5 * amr->BoxSize[2];  // centered in Z
-
-   const double BulkVelX = 150.0;     // km/s bulk motion
-   const double VelDisp  = 1.0;       // km/s velocity dispersion (cold)
-
-   // Initialize RNG locally
-   #ifdef SUPPORT_GSL
-   const gsl_rng_type *T;
-   gsl_rng *rng;
-   gsl_rng_env_setup();
-   T   = gsl_rng_default;
-   rng = gsl_rng_alloc(T);
-   gsl_rng_set(rng, 314); // fixed seed for reproducibility
-   #endif
-
-   for (int ix = 0; ix < Stream_Nx; ix++)
-   for (int iz = 0; iz < Stream_Nz; iz++) {
-      const double x_kpc = x0 + (ix + 0.5) * dx;
-
-      #ifdef SUPPORT_GSL
-      const double dy = gsl_ran_gaussian(rng, Stream_Height / 2.0);
-      const double dz = gsl_ran_gaussian(rng, Stream_Thickness / 2.0);
-      const double dVx = gsl_ran_gaussian(rng, VelDisp);
-      const double dVy = gsl_ran_gaussian(rng, VelDisp);
-      const double dVz = gsl_ran_gaussian(rng, VelDisp);
-      #else
-      const double dy = 0.0;
-      const double dz = 0.0;
-      const double dVx = 0.0;
-      const double dVy = 0.0;
-      const double dVz = 0.0;
-      #endif
-
-      ParFltData_AllRank[PAR_MASS][p]  = 0.0;
-      ParFltData_AllRank[PAR_POSX][p]  = real_par(x_kpc);
-      ParFltData_AllRank[PAR_POSY][p]  = real_par(y0 + dy);
-      ParFltData_AllRank[PAR_POSZ][p]  = real_par(z0 + dz);
-
-      ParFltData_AllRank[PAR_VELX][p]  = real_par(BulkVelX + dVx);
-      ParFltData_AllRank[PAR_VELY][p]  = real_par(dVy);
-      ParFltData_AllRank[PAR_VELZ][p]  = real_par(dVz);
-
-      ParIntData_AllRank[PAR_TYPE][p] = PTYPE_TRACER;
-
-      p++;
-   }
-
    // Free RNG
    #ifdef SUPPORT_GSL
    gsl_rng_free(rng);
    #endif
-}
-
-
 
    } // if ( MPI_Rank == 0 )
 
